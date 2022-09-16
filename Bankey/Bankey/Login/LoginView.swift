56c223a67d98064dc7594a8b8659e87999c027c0
Pritam Dash <pritamdash2015@outlook.com>
2022-09-16 18:52:50 +0530

    Login view set into middle of page.

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginView : UIView {
+    
+    override init(frame : CGRect){
+        super.init(frame : frame)
+            style()
+            layout()
+            
+        }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width : 200, height : 200)
+    }
+    //This  helps with brand new view to size itself and if it is ever put into a stack view this view will know roughly how to size itself.
+}
+
+extension LoginView {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .orange
+    }
+    
+    func layout() {
+        
+    }
+}
--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    //Number 1 thing you have to remember when working with custom views and auto layout is to set the translateAutoresizingMaskIntoConstraints = false
+    // translateAutoresizingMaskIntoConstraints takes any element or control of a view in your view controller and makes it ready for auto layout.
+    let loginView = LoginView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
 
 
+}
+
+extension LoginViewController {
+    private func style()
+    {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        //Do some autolayout constraints
+        
+        //This sets isActive to true for all constraints contained within
+        NSLayoutConstraint.activate([
+            //for mid of the page :
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            //for leading and trailing we use the SystemSpacingAfter multiplier and put spacing of leading here, 1 = 8 points
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+            
+        ])
+    }
 }
 
