1596d739e6493f8f2e962c96e77622f57757b257
Pritam Dash <pritamdash2015@outlook.com>
2022-09-17 21:34:52 +0530

    Sign in button setup

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -12,6 +12,8 @@ class LoginViewController: UIViewController {
     //Number 1 thing you have to remember when working with custom views and auto layout is to set the translateAutoresizingMaskIntoConstraints = false
     // translateAutoresizingMaskIntoConstraints takes any element or control of a view in your view controller and makes it ready for auto layout.
     let loginView = LoginView()
+
+    let signInButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,21 +28,44 @@ extension LoginViewController {
     private func style()
     {
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8 // for indicator spacing
+        signInButton.setTitle("Sign In", for: [])
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton)
         //Do some autolayout constraints
         
         //This sets isActive to true for all constraints contained within
+        
+        //LoginView
         NSLayoutConstraint.activate([
             //for mid of the page :
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             //for leading and trailing we use the SystemSpacingAfter multiplier and put spacing of leading here, 1 = 8 points
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
-            
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+        ])
+        
+        //SignIn Button
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
         ])
     }
+    
+}
+
+extension LoginViewController {
+    @objc func signInTapped() {
+        
+    }
+    
 }
 
